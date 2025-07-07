c5d8e7a60ab18a00ff5b279859df40c5696dd4f6
Suwan <C289031@g.hongik.ac.kr>
2025-07-07 15:23:12 +0900

    feat: @RestController를 통한 전체 메서드 @Responsebody 적용

--- src/main/java/com/metaverse/springmvc/controller/DataResponseController.java
+++ src/main/java/com/metaverse/springmvc/controller/DataResponseController.java
@@ -1,21 +1,18 @@
 package com.metaverse.springmvc.controller;
 
 import com.metaverse.springmvc.domain.star;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class DataResponseController {
     @GetMapping("response/json/string")
-    @ResponseBody
     public String helloStringjson(){
         return "{\"name\" : \"Meta\", \"age\" : 95}";
     }
 
 
     @GetMapping("response/json/instance")
-    @ResponseBody
     public star helloInstancejson(){
         return new star("Meta", 88);
     }
