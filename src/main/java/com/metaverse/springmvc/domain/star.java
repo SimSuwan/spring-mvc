4a8945e8bb7353b4c267244b84be282869578d8b
Suwan <C289031@g.hongik.ac.kr>
2025-07-07 15:44:36 +0900

    feat: json to object, 잭슨 라이브러리가 수행 / 기본 생성자, 게터 필요

--- src/main/java/com/metaverse/springmvc/domain/star.java
+++ src/main/java/com/metaverse/springmvc/domain/star.java
@@ -1,14 +1,14 @@
 package com.metaverse.springmvc.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class star {
     String name;
     int age;
 
-    public star(String name, int age){
-        this.name = name;
-        this.age = age;
-    }
 }
--- src/test/java/com/metaverse/springmvc/jsontest.java
+++ src/test/java/com/metaverse/springmvc/jsontest.java
@@ -13,9 +13,22 @@ public class jsontest {
     void test1() throws JsonProcessingException {
         star star = new star("Meta", 55);
 
+        // Jackson 라이브러리의 objectMapper가 파싱 기능을 수행
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonString = objectMapper.writeValueAsString(star);
 
         System.out.println("json result : " + jsonString);
+    }
+
+    @Test
+    @DisplayName("")
+    void test2() throws JsonProcessingException{
+        String json = "{\"name\" : \"Suwan\", \"age\" : 23}";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        // json 문자열을 star 클래스에 맞추어 값을 읽고 반환
+        star star = objectMapper.readValue(json, star.class);
+        System.out.println("name: " + star.getName());
+        System.out.println("age: " + star.getAge());
     }
 }
